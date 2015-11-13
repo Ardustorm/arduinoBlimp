16de1f31fcb971b5e3b351c39c060a76d796f707
Ardustorm <luke24601@gmail.com>
2015-11-13 15:51:56 -0800

    Adds fine control of motors and servos through serial.

--- BlimpMotorTest/BlimpMotorTest.ino
+++ BlimpMotorTest/BlimpMotorTest.ino
@@ -11,17 +11,53 @@ const int yawServoPin = 5;
 Servo pitchServo;
 const int pitchServoPin = 6;
 
+//Speed and location variables for servos and motor
+int motorSpeed = 0;
+int yawPositon = 90;
+
 void setup()
 {
+  Serial.begin(9600);
   yawServo.attach(yawServoPin);
 
   pinMode(motorPin1, OUTPUT); // set up for h bridge pin connection
   pinMode(motorPin2, OUTPUT);
+
+  Serial.println("Code Starts now");
+  Serial.flush();
 }
 
 void loop()
 {
-  if (Serial.available() > 0)
+  //characterControl();   //Original Control scheme using letters to control motor/servo
+  numberControl();
+
+
+}
+
+void numberControl()
+{
+
+  Serial. println("Enter Motor Speed (-250 to 250), then press enter:");
+  while(Serial.available() == 0);   // Wait here until input buffer has a character
+  motorSpeed = Serial.parseInt();
+  Serial.print("motorSpeed = "); Serial.println(motorSpeed,DEC);
+  Serial.print("Yaw Position = "); Serial.println(yawPositon,DEC);
+
+  Serial. println("Enter Servo Position (0 to 180), then press enter:");
+  while(Serial.available() == 0);   // Wait here until input buffer has a character
+  yawPositon = Serial.parseInt();
+  Serial.print("motorSpeed = "); Serial.println(motorSpeed,DEC);
+  Serial.print("Yaw Position = "); Serial.println(yawPositon,DEC);
+
+
+  drive(motorSpeed);
+  steer(yawPositon,0);
+  
+}
+void characterControl()
+{
+    if (Serial.available() > 0)
   {
     int ByteReceived = Serial.read(); // reads from console
     Serial.print(ByteReceived);   // prints input back to terminal
@@ -65,7 +101,6 @@ void loop()
   // END Serial Available
   }
 }
-
 
 void drive(int speed)     //-250 to 250
 {
